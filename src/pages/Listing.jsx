a9a675602ac390bf2bc7c0e50a6d24c311181d7e
Andre <andregozubuyuk@gmail.com>
2024-02-06 22:46:04 +0300

    Updated files

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -16,6 +16,7 @@ import { FaShare, FaBed, FaBath, FaParking, FaChair } from "react-icons/fa"
 import { FaMapMarkerAlt } from "react-icons/fa"
 import { getAuth } from "firebase/auth"
 import Contact from "../components/Contact"
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 
 export default function Listing() {
   const params = useParams()
@@ -141,7 +142,24 @@ export default function Listing() {
             <Contact userRef={listing.userRef} listing={listing} />
           )}
         </div>
-        <div className="w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+        <div className="w-full h-[200px] lg-[400px] z-10 overflow-x-hidden">
+          <MapContainer
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>{listing.address}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       </div>
     </main>
   )
