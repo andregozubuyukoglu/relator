a56e6660e56f8e6a7f0681d77e59fcff1c98fe65
Andre <andregozubuyuk@gmail.com>
2024-02-07 23:25:21 +0300

    Updated files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,13 @@
+import { useEffect, useState } from "react"
+import Slider from "../components/Slider"
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore"
+import { db } from "../firebase"
+import Spinner from "../components/Spinner"
+
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <div>
+      <Slider />
+    </div>
+  )
 }
